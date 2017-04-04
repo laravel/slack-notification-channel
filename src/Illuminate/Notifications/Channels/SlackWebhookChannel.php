2eeb658a677661651f2dd4a678ad69138be2bb71
Kenneth <kkeegan@gmail.com>
2017-04-04 11:13:15 -0700

    Support for image_url property within Slack attachments

--- src/Illuminate/Notifications/Channels/SlackWebhookChannel.php
+++ src/Illuminate/Notifications/Channels/SlackWebhookChannel.php
@@ -90,6 +90,7 @@ class SlackWebhookChannel
                 'footer' => $attachment->footer,
                 'footer_icon' => $attachment->footerIcon,
                 'ts' => $attachment->timestamp,
+                'image_url' => $attachment->imageUrl,
             ]);
         })->all();
     }
