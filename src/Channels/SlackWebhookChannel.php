b275909b6037e56d4be594b7d6e915df30ebd5f6
Bader Almutairi <phpfalcon@gmail.com>
2020-02-10 23:28:46 +0300

    Update SlackWebhookChannel.php

--- src/Channels/SlackWebhookChannel.php
+++ src/Channels/SlackWebhookChannel.php
@@ -52,7 +52,7 @@ class SlackWebhookChannel
      * @param  \Illuminate\Notifications\Messages\SlackMessage  $message
      * @return array
      */
-    protected function buildJsonPayload(SlackMessage $message)
+    public function buildJsonPayload(SlackMessage $message)
     {
         $optionalFields = array_filter([
             'channel' => data_get($message, 'channel'),
