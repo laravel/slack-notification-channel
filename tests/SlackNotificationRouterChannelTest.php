0cff97b898a99368ce54ba9004e21844ccd187ab
TomaszOnePilot <tomasz@onepilot.co>
2024-08-17 15:51:48 +0200

    fix: do not notify when route is empty

--- tests/SlackNotificationRouterChannelTest.php
+++ tests/SlackNotificationRouterChannelTest.php
@@ -76,6 +76,9 @@ class SlackNotificationRouterChannelTest extends TestCase
         $channel = new SlackNotificationRouterChannel($app);
 
         $this->assertEquals(null, $channel->send(new SlackChannelTestNotifiable(false), new SlackChannelTestNotification()));
+        $this->assertEquals(null, $channel->send(new SlackChannelTestNotifiable(null), new SlackChannelTestNotification()));
+        $this->assertEquals(null, $channel->send(new SlackChannelTestNotifiable(''), new SlackChannelTestNotification()));
+        $this->assertEquals(null, $channel->send(new SlackChannelTestNotifiable([]), new SlackChannelTestNotification()));
     }
 }
 
